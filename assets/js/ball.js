70fee8f8a51b2616fe047ec2b9fce1fc507eeafb
conto <constantijn@vanhartesveldt.nl>
2021-04-27 09:46:15 +0200

    commit

--- assets/js/ball.js
+++ assets/js/ball.js
@@ -10,4 +10,30 @@ class Ball extends Rectangle {
         this.velocity.x = (getRandomNumBetween(0, 1) === 1) ? getRandomNumBetween(-50, -150) : getRandomNumBetween(50, 150);
         this.velocity.y = (getRandomNumBetween(0, 1) === 1) ? getRandomNumBetween(-50, -150) : getRandomNumBetween(50, 150);
     }
+    reset(x = this.startX, y = this.startY) {
+        this.setPosition(x, y);
+        this.setSpeed(150);
+        this.setAngle(getRandomNumBetween(1, 2) === 1 ? getRandomNumBetween(150, 210) : getRandomNumBetween(330, 390));
+        this.out = false;
+    }
+    setPostition(x, y) {
+        this.position.x = x;
+        this.position.y = y;
+    }
+    setSpeed(speed) {
+        this.speed = Math.ceil(speed);
+        console.log("setSpeed(" + this.speed + ")");
+        console.log("velocity =" + this.velocity.x, this.velocity.y);
+    }
+    setAngle(degrees) {
+        const radians = this.toRadians(degrees);
+        this.velocity.x = this.speed * Math.cos(radians);
+        this.velocity.y = this.speed * -Math.sin(radians);
+    }
+    toRadians(degrees) {
+        return degrees * (Math.pi / 180)
+    }
+    toDegrees(radians) {
+        return (radians * 180) / Math.pi
+    }
 }
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,96 +1,187 @@
+
 class Game {
-    constructor() {
-        this.canvas = document.getElementById('pong');
-        this.context = this.canvas.getContext('2d')
+	constructor() {
+)
+		this.canvas = document.getElementById("pong");
+		this.context = this.canvas.getContext("2d");
 
-        this.ball = new Ball(this.canvas.width / 2, this.canvas.height / 2, 'orange');
-        this.players = [
-            new Player(20, this.canvas.height / 2, 1),
-            new Player(this.canvas.width - 20, this.canvas.height / 2, 2)
-        ];
-        this.keys = [];
-        window.addEventListener('keydown', (Event) => {
-            switch (Event.detail) {
-                case 'w':
-                    this.keys[38] = true;
-                    break;
-                case 'a':
-                    this.keys[40] = true;
-                    break;
+		this.ball = new Ball(this.canvas.width / 2, this.canvas.height / 2, "green");
 
-            }
-        });
-        let lastTime;
-        const callback = (milliseconds) => {
-            if (lastTime) {
-                this.update((milliseconds - lastTime) / 1000);
-                this.draw();
-            }
-            lastTime = milliseconds;
-            window.requestAnimFrame(callback);
-        }
-        callback();
-    }
 
-    checkInput(player, ball) {
-        switch (player.id) {
-            case 1: // Human player (links)
-                player.velocity.y = 0;
-                player.velocity.y += (this.keys[38] === true) ? -400 : 0;
-                player.velocity.y += (this.keys[40] === true) ? 400 : 0;
-                break;
-            case 2: // Computer player (rechts)
-                player.position.y = ball.position.y;
-                break;
-        }
-    }
-    update(deltatime) {
-        this.ball.position.x += this.ball.velocity.x * deltatime;
-        this.ball.position.y += this.ball.velocity.y * deltatime;
+		this.players = [new Player(20, this.canvas.height / 2, 1), new Player(this.canvas.width - 20, this.canvas.height / 2, 2)];
 
-        this.players[0].position.y += this.players[0].velocity.y * deltatime;
-        this.players[1].position.y += this.players[1].velocity.y * deltatime;
 
-        this.players.forEach(player => this.checkInput(player, this.ball));
-        this.checkCollisions(this.players, this.ball, this.hud.edges, deltatime);
-    }
+		this.hud = new Hud(this);
 
-    checkCollisions(players, ball, edges, deltatime) {
-        // Controleer of de bedjes van speler1 en speler2 tegen de randen aankomen
-        for (let p = 0; p < players.length; p++) {
-            if (players[p].top < edges[0].bottom) {
-                players[p].position.y = edges[0].size.y + (players[p].size.y / 2);
-            } else if (players[p].bottom > edges[1].top) {
-                players[p].position.y = this.canvas.height - edges[1].size.y - (players[p].size.y / 2);
-            }
-        }
-        if (this.ball.bottom > this.canvas.height || this.ball.top < 0) {
-            this.ball.velocity.y = -this.ball.velocity.y;
-        }
-        if (this.collide(ball, players[0], deltatime)) {
-            alert('bots!');
-        }
-    }
-}
+		this.keys = [];
 
-collide(rect1, rect2, dt) {
-    if (rect1.left + rect1.velocity.x * dt < rect2.right + rect2.velocity.x * dt &&
-        rect1.right + rect1.velocity.x * dt > rect2.left + rect2.velocity.x * dt &&
-        rect1.top + rect1.velocity.y * dt < rect2.bottom + rect2.velocity.y * dt &&
-        rect1.bottom + rect1.velocity.y * dt > rect2.top + rect2.velocity.y * dt) {
-        return true;
-    } else {
-        return false;
-    }
-}
-draw() {
-    this.drawRectangle(this.context, this.ball);
-    for (let i = 0; i < this.players.length; i++) {
-        this.drawRectangle(this.context, this.players[i]);
-    }
-}
-drawRectangle(ctx, rect, color = 'white') {
-    ctx.fillStyle = color;
-    ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
-}
+        // 2 event listeners voor de bestuuring van player 1 want anders pakt die hem niet goed
+		window.addEventListener("KEY_DOWN", (event) => {
+			switch (event.detail) {
+				case "ArrowUp":
+					this.keys[38] = true;
+					break;
+				case 38:
+					this.keys[38] = true;
+					break;
+				case "ArrowDown":
+					this.keys[40] = true;
+					break;
+				case 40:
+					this.keys[40] = true;
+					break;
+			}
+		});
+
+		window.addEventListener("KEY_UP", (event) => {
+			switch (event.detail) {
+				case "ArrowUp":
+					this.keys[38] = false;
+					break;
+				case 38:
+					this.keys[38] = false;
+					break;
+				case "ArrowDown":
+					this.keys[40] = false;
+					break;
+				case 40:
+					this.keys[40] = false;
+					break;
+			}
+		});
+
+		let lastTime;
+		const callback = (milliseconds) => { // 60 hertz gameloop
+			if (lastTime) {
+				this.update((milliseconds - lastTime) / 1000);
+				this.draw();
+			}
+			lastTime = milliseconds;
+			window.requestAnimFrame(callback);
+		};
+		callback();
+	}
+
+	checkInput(player, ball) {
+		switch (player.id) { // case 1 is mens case 2 is een bot of tewel 
+			case 1: 
+				player.velocity.y = 0;
+				player.velocity.y += this.keys[38] === true ? -400 : 0;
+				player.velocity.y += this.keys[40] === true ? 400 : 0;
+				break;
+			case 2: 
+				if (player.locked) {
+					if (--player.stickyFrames === 0) {
+						player.velocity.y = 0;
+						player.locked = false;
+						
+					}
+					return;
+				} else {
+					player.velocity.y = 0;
+					player.velocity.y += ball.position.y < player.top ? -400 : 0;
+					player.velocity.y += ball.position.y > player.bottom ? 400 : 0;
+					player.stickyFrames = getRandomNumBetween(5, 20);
+					player.locked = true;
+					
+				}
+				break;
+		}
+	}
+
+	update(deltatime) {
+		if (this.ball.locked) return;
+
+		this.ball.position.x += this.ball.velocity.x * deltatime;
+		this.ball.position.y += this.ball.velocity.y * deltatime;
+
+		this.players[0].position.y += this.players[0].velocity.y * deltatime;
+		this.players[1].position.y += this.players[1].velocity.y * deltatime;
+
+		this.players.forEach((player) => this.checkInput(player, this.ball));
+
+		this.checkCollisions(this.players, this.ball, this.hud.edges, deltatime);
+	}
+
+	checkCollisions(players, ball, edges, deltatime) {
+		for (let p = 0; p < players.length; p++) {
+			if (players[p].top < edges[0].bottom) {
+				players[p].position.y = edges[0].size.y + players[p].size.y / 2;
+			} else if (players[p].bottom > edges[1].top) {
+				players[p].position.y = this.canvas.height - edges[1].size.y - players[p].size.y / 2;
+			}
+		}
+
+		if (ball.bottom > this.canvas.height - 10 || ball.top < 10) {
+			ball.velocity.y = -ball.velocity.y;
+		}
+
+		if (ball.out) return;
+
+		if (ball.left + ball.velocity.y * deltatime < players[0].right || ball.right - ball.velocity.y * deltatime > players[1].left) {
+			const player = ball.position.x < this.canvas.width / 2 ? players[0] : players[1];
+			if (this.collide(ball, player, deltatime)) {
+				if (ball.bottom > player.top && ball.top < player.bottom) {
+					console.log("frontale botsing gedetecteerd");
+					ball.position.x = player.id === 1 ? player.right + ball.size.y / 2 : player.left - ball.size.y / 2;
+					ball.velocity.x = -ball.velocity.x;
+
+                    const ballY    = ball.position.y   | 0;
+                    const playerY  = player.position.y | 0;
+                    const distance = ballY < playerY ? Math.abs(ballY - playerY) : -Math.abs(ballY - playerY);
+
+                    console.log( "afstand vanaf midden van bedje: "+distance );
+
+                    if(distance !== 0)  ball.setAngle( player.id===1 ? distance : -distance+180 ); 
+
+                    Math.floor()
+                    Math.random()
+                    Math.ceil()
+                    Math.PI 
+
+                    ball.setSpeed( ball.speed*1.1 );
+				} else if (ball.position.y < player.position.y) {
+					console.log("botsing aan de bovenkant van speler gedetecteerd");
+					ball.position.y = player.top - ball.size.y / 2;
+					ball.velocity.y = player.velocity.y < 0 && player.velocity.y < ball.velocity.y ? player.velocity.y * 1.1 : -ball.velocity.y;
+				} else if (ball.position.y > player.position.y) {
+					console.log("botsing aan de onderkant van speler gedetecteerd");
+					ball.position.y = player.bottom + ball.size.y;
+					ball.velocity.y = player.velocity.y > 0 && player.velocity.y > ball.velocity.y ? player.velocity.y * 1.1 : -ball.velocity.y;
+				}
+			}
+
+			
+			if (ball.right < 0 || ball.left > this.canvas.width) {
+				this.hud.addScore(player.id === 1 ? 2 : 1);
+				ball.out = true;
+				setTimeout(() => {
+				// 	this.ball.reset();
+				// }, 1000);
+			}
+		}
+
+	
+	}
+
+	collide(rect1, rect2, dt) {
+		if (rect1.left + rect1.velocity.x * dt < rect2.right + rect2.velocity.x * dt && rect1.right + rect1.velocity.x * dt > rect2.left + rect2.velocity.x * dt && rect1.top + rect1.velocity.y * dt < rect2.bottom + rect2.velocity.y * dt && rect1.bottom + rect1.velocity.y * dt > rect2.top + rect2.velocity.y * dt) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	draw() {
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		this.drawRectangle(this.context, this.ball);
+		for (let i = 0; i < this.players.length; i++) {
+			this.drawRectangle(this.context, this.players[i]);
+		}
+	}
+
+	drawRectangle(ctx, rect, color = "white") {
+		ctx.fillStyle = color;
+		ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
+	}
 }
